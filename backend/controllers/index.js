d8f4362842d91f1bb9ade0ff0ab9996f8fe31389
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-11-18 02:58:19 +0530

    save trello

---
+++ backend/controllers/index.js
@@ -0,0 +1,27 @@
+async function createTask(req, res) {
+    
+}
+
+async function updateTask(req, res) {
+    
+}
+
+async function deleteTask(req, res) {
+    
+}
+
+async function getTask(req, res) {
+    
+}
+
+async function newBoard(req, res) {
+    
+}
+
+export {
+    createTask,
+    updateTask,
+    deleteTask,
+    getTask,
+    newBoard
+}
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,6 @@
 import express from "express";
 const router = express.Router();
+import { createTask, updateTask, deleteTask, getTask, newBoard } from "../controllers/index.js";
 
 
 
