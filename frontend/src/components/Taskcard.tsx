3ec0ada9d97134c7a76229530ad2dfa8184dc188
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-11-18 00:26:22 +0530

    card

---
+++ frontend/src/components/Taskcard.tsx
@@ -0,0 +1,33 @@
+import { BsArrowsAngleContract } from "react-icons/bs";
+import { BsThreeDots } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa6";
+import { IoImages } from "react-icons/io5";
+const TaskCard = () => {
+    return (
+        <div className="bg-yellow-200  max-w-72 p-4 rounded-2xl">
+            <div className="flex justify-between items-center ">
+                <p className="ml-1 font-bold text-sm">Today</p>
+                <div className="flex flex-row gap-4 justify-between">
+                    <BsArrowsAngleContract size={15} fill="black" className="rotate-45" />
+                    <BsThreeDots size={15} fill="black" className="text-white" />
+                </div>
+
+            </div>
+            <div className="flex flex-row justify-between mt-4">
+                <div className="flex flex-row gap-2 items-center  cursor-pointer hover:bg-yellow-300 rounded-lg w-full p-2 ">
+
+
+                    <FaPlus size={14} fill="black" className="text-white" />
+                    <p className="text-sm font-semibold text-gray-600">Add a card</p>
+                </div>
+                <div className="flex flex-row gap-2 items-center cursor-pointer ml-4 rounded-lg hover:bg-yellow-300 p-2 px-3">
+                    <IoImages size={15} fill="black" className="text-white" />
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+export default TaskCard;
--- frontend/src/components/Trelloboard.tsx
+++ frontend/src/components/Trelloboard.tsx
@@ -1,9 +1,10 @@
+import TaskCard from "./Taskcard"
 
 
 const TrelloBoard = () => {
   return (
     <div className="h-[100vh] bg-gradient-to-b from-violet-500 to-pink-500">
-      TrelloBoard
+       <TaskCard/>
     </div>
   )
 
